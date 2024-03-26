8fa9634437c48fb4a3ae75b06ef6900c194a1240
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-03-26 01:17:27 +0100

    first commit

--- index.js
+++ index.js
@@ -22,16 +22,12 @@ app.use(express.json()); // parse data to json format
 //   origin: [process.env.ORIGIN],
 // };
 
-const corsConfig = {
-  credentials: true, // this allows to send back (to client) cookies
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "https://student-app-y.vercel.app",
-  ],
-  preflightContinue: false,
-};
+// const corsConfig = {
+//   credentials: true, // this allows to send back (to client) cookies
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+//   origin: ["http://localhost:3000", "http://localhost:3001", ""],
+//   preflightContinue: false,
+// };
 
 app.use(cors(corsConfig));
 
